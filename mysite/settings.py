34a098e37b703d06c699b139aa2a1d1b34690e40
Maria Milkowski <mariamilkowski@marias-mbp.dhcp.nd.edu>
2026-04-27 10:04:48 -0400

    Amazon S3 Changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'accounts',
     'skillsessions',
     'django.contrib.sites',
-
+    "storages",
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
@@ -193,3 +193,16 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     }
 }
+
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+AWS_STORAGE_BUCKET_NAME = "prond-uploads-2026"
+
+AWS_S3_REGION_NAME = "us-east-1"
+
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
